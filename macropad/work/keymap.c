ee9de263a29664f6f3708e9ed9a9800332409e30
Paul Landers <planders@statestitle.com>
2021-10-25 15:42:54 -0400

    macropad: jump to space

--- macropad/work/keymap.c
+++ macropad/work/keymap.c
@@ -8,11 +8,14 @@
 #define D_COPY  LGUI(KC_C)
 #define D_PASTE LGUI(KC_V)
 // #define D_TODO  LCTL(KC_SPC)
+#define D_SPC_1  LCTL(KC_1)
+#define D_SPC_2  LCTL(KC_2)
+#define D_SPC_3  LCTL(KC_3)
 // #define D_DASH  LALT(KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_macro(
-    D_SPC_L, D_TAB_L, Z_ALTTB, D_TAB_R, D_SPC_R, \
+    D_SPC_1, D_SPC_2, D_SPC_3, D_TAB_L, D_TAB_R, \
     D_HIDE, Z_ALTTB, D_ALFRD, D_COPY, D_PASTE, \
     KC_MRWD, KC_MPLY, KC_MFFD, D_SLACK, D_ZOOM  \
   ),
