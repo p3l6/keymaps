c1b4d93cfa91fea1df533eb63d8ece81304751eb
Paul Landers <paul@srednal.com>
2022-04-21 09:35:47 -0400

    Add alternate bases

--- bmek/keymap.c
+++ bmek/keymap.c
@@ -3,12 +3,18 @@
 #include "caps-word.h"
 
 #define _BASE 0
-#define _NAV 1
-#define _SYMBL 2
-#define _MEDIA 3
+#define _GRAV 1 // a secondary base layer that changes underscore -> grave accent
+#define _PLAIN 2 // a secondary base layer that hides all special functions
+#define _NAV 3
+#define _SYMBL 4
+#define _MEDIA 5
 
 #define LT_SYMB  LT(_SYMBL, KC_ENT)
 #define LT_MEDIA LT(_MEDIA, KC_GRV)
+
+#define BASE_B DF(_BASE)
+#define BASE_G DF(_GRAV)
+#define BASE_P DF(_PLAIN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -20,9 +26,25 @@ D_SLACK, LT_SYMB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     
 D_ZOOM,  KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_UNDS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSPC, LT_MEDIA,
                   KC_LALT, KC_LGUI,          MO(_NAV),                 KC_SPC,           KC_RGUI, KC_RCTL
 
+),[_GRAV] = LAYOUT_hhkb(
+
+_______, _______, _______, _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______, _______, _______, _______,
+_______, _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______, _______, _______,
+_______, _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______, _______,
+_______, _______, _______, _______, _______, _______, _______,     LALT(KC_GRV), _______, _______, _______, _______, _______,          _______, _______,
+                  _______, _______,          _______,                  _______,          _______, _______
+
+),[_PLAIN] = LAYOUT_hhkb(
+
+__xxx__, KC_ESC,  _______, _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______, _______, _______, _______,
+__xxx__, _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______, _______, _______,
+__xxx__, __xxx__, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______, _______,
+__xxx__, KC_LSFT, _______, _______, _______, _______, _______,          KC_DOT, _______, _______, _______, _______, _______,          KC_RSFT, DF(_BASE),
+                  _______, _______,          KC_SPC,                  _______,          _______, _______
+
 ),[_NAV] = LAYOUT_hhkb(
 
-__xxx__, KC_ESC,  D_SPC_1, D_SPC_2, D_SPC_3, D_SPC_4, __xxx__, __xxx__,         __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, _______,
+RESET,   KC_ESC,  D_SPC_1, D_SPC_2, D_SPC_3, D_SPC_4, __xxx__, __xxx__,         __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, _______,
 __xxx__, KC_DEL,  D_SUBWD, Z_ALTTB, KC_UP,   D_EXPOS, D_SPC_L,                  D_SPC_R, __xxx__, KC_UP,   __xxx__, KC_PGUP, KC_HOME, __xxx__, _______,
 __xxx__, KC_BSPC, KC_LALT, KC_LEFT, KC_DOWN, KC_RGHT, D_TAB_L,                  D_TAB_R, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_END,           KC_PENT,
 __xxx__, KC_LSFT, KC_LGUI, _______, _______, D_GRP_U, D_GRP_L,         D_GRP_R, D_GRP_D, __xxx__, __xxx__, __xxx__, __xxx__,          KC_RSFT, __xxx__,
@@ -31,17 +53,17 @@ __xxx__, KC_LSFT, KC_LGUI, _______, _______, D_GRP_U, D_GRP_L,         D_GRP_R, 
 ),[_SYMBL] = LAYOUT_hhkb(
 
 __xxx__, KC_ESC,  D_DBG_C, D_DBG_N, D_DBG_S, D_DBG_O, __xxx__, __xxx__,         __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, _______,
-__xxx__, _______, __xxx__, __xxx__, D_LIN_U, D_CUR_U, __xxx__,                  KC_PIPE, KC_EXLM, KC_EQL,  KC_PLUS, Z_SFRMT, Z_STCMT, Z_ENCMT, _______,
-__xxx__, _______, __xxx__, __xxx__, D_LIN_D, D_CUR_D, D_CUR_A,                  KC_HASH, KC_LCBR, KC_RCBR, KC_MINS, KC_ASTR, KC_GRV,           KC_PENT,
-__xxx__, KC_LSFT, __xxx__, D_LIN_X, D_LIN_N, D_CUR_N, __xxx__,         Z_MTODO, KC_AMPR, KC_DLR,  KC_TILD, Z_ARROW, KC_BSLS,          _______, __xxx__,
+BASE_P,  _______, __xxx__, __xxx__, D_LIN_U, D_CUR_U, __xxx__,                  KC_PIPE, KC_EXLM, KC_EQL,  KC_PLUS, Z_SFRMT, Z_STCMT, Z_ENCMT, _______,
+BASE_G,  _______, __xxx__, __xxx__, D_LIN_D, D_CUR_D, D_CUR_A,                  KC_HASH, KC_LCBR, KC_RCBR, KC_MINS, KC_ASTR, KC_GRV,           KC_PENT,
+BASE_B,  KC_LSFT, __xxx__, D_LIN_X, D_LIN_N, D_CUR_N, __xxx__,         Z_MTODO, KC_AMPR, KC_DLR,  KC_TILD, Z_ARROW, KC_BSLS,          _______, __xxx__,
                   __xxx__, __xxx__,          KC_SPC,                   _______,          __xxx__, __xxx__
 
 ),[_MEDIA] = LAYOUT_hhkb(
 
-__xxx__, RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,           KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F13,  KC_F14,
-__xxx__, __xxx__, D_BRT_D, D_BRT_U, __xxx__, __xxx__, __xxx__,                  __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__,
+__xxx__, __xxx__,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,           KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F13,  KC_F14,
+__xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__,                  __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__,
 __xxx__, __xxx__, KC_MUTE, KC_VOLD, KC_VOLU, __xxx__, __xxx__,                  __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__,          __xxx__,
-__xxx__, KC_CAPS, KC_MRWD, KC_MPLY, KC_MFFD, __xxx__, __xxx__,         _______, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__,          _______, _______,
+__xxx__, KC_CAPS, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__,         _______, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__,          _______, _______,
                   __xxx__, __xxx__,          __xxx__,                  _______,          __xxx__, __xxx__
 
 ),
@@ -77,15 +99,20 @@ void led_set_user(uint8_t usb_led) {
     (layer_state & (1<<_SYMBL)) ? 2 : // Symb
     (layer_state & (1<<_MEDIA)) ? 3 : // Media
     (usb_led & (1 << USB_LED_CAPS_LOCK)) ? 4 : // Caps
+    (default_layer_state & (1 << _GRAV)) ? 5 : // Grav
+    (default_layer_state & (1 << _PLAIN)) ? 6 : // Plain
     0;  // Off
 
   uint8_t hue_pallets[][6] = {
+  // These are byte hues, so <hue degrees>*255/360
   //    0    1    2    3    4    5       LED layout:   5 4 3   (bottom row keys)   0 1 2
     {   0,   0,   0,   0,   0,   0 }, // Off
     { 150,  25, 150, 150,  25, 150 }, // Nav
     { 135, 215,  85, 135, 215,  85 }, // Symb
     {  45,   0,   0,   0,  45,  45 }, // Media
     { 200,   0,   0, 200,   0,   0 }, // Caps
+    {   0,   0,   0,   0, 170,   0 }, // Grav
+    {   0,   0,   0, 170,   0,   0 }, // Plain
   };
 
   if ( mode ) { rgblight_enable_noeeprom(); rgblight_mode(RGBLIGHT_MODE_STATIC_LIGHT); } else { rgblight_disable_noeeprom(); return; }
