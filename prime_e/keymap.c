cb2be375c0deafdb4dc611eafd91dc391f2bf646
Paul Landers <paul@thelanders.family>
2023-08-07 14:52:07 -0400

    Change number layer activator key

--- prime_e/keymap.c
+++ prime_e/keymap.c
@@ -14,8 +14,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT(
 QK_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_DEL, \
 LT_SYMB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,  \
-KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_UNDS,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,  \
-KC_LCTL, KC_LALT,                   KC_LGUI, MO(NAV), KC_SPC,            KC_RGUI,          OSL(NUM),         MO(CODE)   \
+KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    OSL(NUM), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,  \
+KC_LCTL, KC_LALT,                   KC_LGUI, MO(NAV), KC_SPC,            KC_RGUI,          KC_RALT,          MO(CODE)   \
 
 
 ),[NAV] = LAYOUT(
@@ -27,21 +27,21 @@ KC_LCTL, KC_LALT,                   KC_LGUI, _______, KC_SPC,            KC_RGUI
 
 ),[SYMB] = LAYOUT(
 KC_GRV,  KC_LT,   KC_GT,   KC_LBRC, KC_RBRC, Z_MTODO,           KC_AT,   KC_EXLM, KC_PIPE, KC_ASTR, KC_CIRC, _______, _______, \
-_______, KC_QUES, KC_DLR,  KC_LCBR, KC_RCBR, Z_DLR_0,           KC_HASH, KC_EQL,  KC_MINS, KC_PLUS, KC_GRV,  KC_PENT, \
-KC_LSFT, __xxx__, KC_BSLS, KC_LPRN, KC_RPRN, Z_ARROW, KC_PERC,  KC_AMPR, KC_DLR,  KC_SCLN, KC_COLN, KC_BSLS, KC_RSFT,  \
+_______, __xxx__, KC_DLR,  KC_LCBR, KC_RCBR, KC_UNDS,           KC_HASH, KC_EQL,  KC_MINS, KC_PLUS, KC_GRV,  KC_PENT, \
+KC_LSFT, KC_QUES, KC_BSLS, KC_LPRN, KC_RPRN, Z_ARROW, KC_PERC,  KC_AMPR, KC_DLR,  KC_SCLN, KC_COLN, KC_BSLS, KC_RSFT,  \
 _______, _______,                   _______, _______, KC_SPC,            _______,          __xxx__,          __xxx__    \
 
 
 ),[NUM] = LAYOUT(
-KC_0,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,              KC_F9,   KC_F10,  KC_F11,  KC_ASTR, __xxx__, _______, _______, \
-__xxx__, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,              __xxx__, __xxx__, KC_MINS, KC_PLUS, __xxx__, KC_PENT, \
-KC_LSFT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,   KC_F8,   _______, _______, _______, __xxx__,  \
+KC_0,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,              KC_AT,   KC_EXLM, KC_PIPE, KC_ASTR, __xxx__, _______, _______, \
+__xxx__, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,              KC_HASH, KC_EQL,  KC_MINS, KC_PLUS, __xxx__, KC_PENT, \
+KC_LSFT, KC_COLN, KC_DLR,  KC_DOT,  KC_SLSH, KC_UNDS, _______,  KC_AMPR, KC_DLR,  _______, _______, _______, __xxx__,  \
 _______, _______,                   _______, _______, _______,           _______,          _______,          __xxx__    \
 
 
 ),[CODE] = LAYOUT(
-KC_ESC,  D_FORCQ, __xxx__, T_PLNXT, KC_VOLD, KC_VOLU,           __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, QK_BOOT, \
-__xxx__, __xxx__, __xxx__, Z_DEL_W, __xxx__, __xxx__,           __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, \
+KC_ESC,  D_FORCQ, __xxx__, T_PLNXT, KC_VOLD, KC_VOLU,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   __xxx__, QK_BOOT, \
+__xxx__, __xxx__, __xxx__, Z_DEL_W, __xxx__, __xxx__,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  __xxx__, \
 __xxx__, __xxx__, __xxx__, D_SSHOT, __xxx__, __xxx__, __xxx__,  __xxx__, D_DBG_C, D_DBG_N, D_DBG_S, D_DBG_O, KC_CAPS,  \
 __xxx__, __xxx__,                   __xxx__, _______, __xxx__,           __xxx__,          __xxx__,          __xxx__    \
 
